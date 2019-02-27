3ae6deea91db05d7ffb5e3e28ad1329ee778760c
SlyAtBest <slyatbest@gmail.com>
2019-02-27 15:16:36 +0000

    Added a more descriptive comment to explain the current limitations of the Scenario SDK and why we don't explicitly favour one status over another if a single step is run multiple times with differing results

--- scenarioColors.js
+++ scenarioColors.js
@@ -30,6 +30,9 @@ const asyncronouslyUpdateTheStatusOfEachStep = (sdkSteps, status) => Promise.all
 // Note - Whilst the test results can feature a step multiple times, the API only appears to return a single instance, so the status will get set multiple times, and if there is 
 // different a statuses the last one set will win  E.g. in one instance the step passed and in another it failed you are at the mercy of the order in which the promises resolve as to
 // which gets set in Scenario.
+// There are several ways where we could actively prioritise setting one status over the other, but I'm not sure which would be more desirable. E.g. If we have a step that ran twice and in one instance it passes and in one it fails. 
+// If we took the passed result we would be masking a failure, but if we took a failed result the output of the scenario where the step passed would be confusing. (It would appear as if it continued to run other steps after a step failed.)
+// This is a limitation of the Scenario SDK and should be raised with Tricentis.
 // @impure: reliant on state object
 const updateStepResults = (name, status) => {
     state.sdk.getSteps(`"${name}"`)
